76acf2a0c2c297c8e4df1d97fe743ea30df922bc
kauagabassumpc08 <132714120+kauagabassumpc08@users.noreply.github.com>
2023-10-06 09:46:05 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let cor;
+let posicaoHorizontal; //x
+let posicaoVertical; //y
+
+function setup() {
+  createCanvas(600, 500);
+background("black")  
+  cor = color(random(0,255),random(0,255),random(0,255));
+posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+  
+  fill(cor)
+  circle(posicaoHorizontal,posicaoVertical,50)
+  
+ if( mouseX < posicaoHorizontal) {
+   posicaoHorizontal = posicaoHorizontal - 2;
+ }
+ if( mouseX > posicaoHorizontal) {
+   posicaoHorizontal =  posicaoHorizontal + 2;
+ }
+  if(mouseY < posicaoVertical) {
+    posicaoVertical--;
+  }
+  if(mouseY > posicaoVertical) {
+    posicaoVertical++;
+  }
+  if(mouseIsPressed) {
+cor = color(random(0,255),random(0,255),random(0,255),random(0,100));
+    
+  }
+  
+  
+  
+  
+  
